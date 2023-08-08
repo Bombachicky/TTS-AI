3219dbbd0034d068b2221fc64cb6192a66ba5bfa
Bombachicky <kristianjeanmichel@gmail.com>
2023-08-08 15:54:01 -0400

    initial commit

---
+++ api/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello from Go with Fiber!")
+	})
+
+	app.Listen(":8080")
+}
