0b25c28a9ec959e22fdee814a5c0c8f3ab5b1429
farbubby <farhan.mahbub@ucf.edu>
2023-08-13 20:14:57 -0400

    created a GET endpoint getting a specific user

--- api/database/db.go
+++ api/database/db.go
@@ -1,62 +1,22 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
-func main() {
+func startDB() (*dynamodb.DynamoDB) {
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
 	}));
-    
+
     // Create DynamoDB client
 	client := dynamodb.New(sess, &aws.Config{Endpoint: aws.String("http://localhost:8000")});
     
     if client == nil {
-        fmt.Println("Could not create DynamoDB client")
-        return
+        return nil
     }
 
-	// Create the input configuration instance
-    input := &dynamodb.ListTablesInput{};
-
-    fmt.Printf("Tables:\n")
-
-    for {
-        // Get the list of tables
-        result, err := client.ListTables(input)
-        if err != nil {
-            if aerr, ok := err.(awserr.Error); ok {
-                switch aerr.Code() {
-                case dynamodb.ErrCodeInternalServerError:
-                    fmt.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
-                default:
-                    fmt.Println(aerr.Error())
-                }
-            } else {
-                // Print the error, cast err to awserr.Error to get the Code and
-                // Message from an error.
-                fmt.Println(err.Error())
-            }
-            return
-        }
-
-        for _, n := range result.TableNames {
-            fmt.Println(*n)
-        }
-
-        // assign the last read tablename as the start for our next call to the ListTables function
-        // the maximum number of table names returned in a call is 100 (default), which requires us to make
-        // multiple calls to the ListTables function to retrieve all table names
-        input.ExclusiveStartTableName = result.LastEvaluatedTableName
-
-        if result.LastEvaluatedTableName == nil {
-            break
-        }
-    }
+	return client
 }
--- api/database/go.mod
+++ api/database/go.mod
@@ -3,6 +3,7 @@ module db
 go 1.20
 
 require (
+	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/aws/aws-sdk-go v1.44.322 // indirect
 	github.com/aws/aws-sdk-go-v2 v1.20.1 // indirect
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.4.12 // indirect
@@ -22,8 +23,19 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.15.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.21.2 // indirect
 	github.com/aws/smithy-go v1.14.1 // indirect
+	github.com/gofiber/fiber/v2 v2.48.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/klauspost/compress v1.16.3 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/net v0.1.0 // indirect
-	golang.org/x/text v0.4.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.48.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
 )
