813f0bd300d69723c203c6106a2739b86ae09319
iluvshi <shian.morrison@gmail.com>
2023-08-13 20:15:23 -0400

    *Fixed form text coloring

--- src/app/components/signInForm.tsx
+++ src/app/components/signInForm.tsx
@@ -26,7 +26,7 @@ export default function SignIn(){
                   type="email"
                   autoComplete="email"
                   required
-                  className="block w-full rounded-md border-0 py-1.5 text-primary shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-btn sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-formText shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-btn sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
@@ -49,7 +49,7 @@ export default function SignIn(){
                   type="password"
                   autoComplete="current-password"
                   required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-formText shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
--- src/app/components/singUpForm.tsx
+++ src/app/components/singUpForm.tsx
@@ -27,7 +27,7 @@ export default function SignUp(){
                   type="email"
                   autoComplete="email"
                   required
-                  className="block w-full rounded-md border-0 py-1.5 text-primary shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-btn sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-formText shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-btn sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
@@ -46,7 +46,7 @@ export default function SignUp(){
                   type="password"
                   autoComplete="current-password"
                   required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-formText shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
