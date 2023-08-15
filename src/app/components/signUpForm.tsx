f891b54130e92c7d7be49852d4f925bc944a7349
Kristian Michel <kristianjeanmichel@gmail.com>
2023-08-15 11:38:56 -0400

    fixed spacing

--- src/app/components/signUpForm.tsx
+++ src/app/components/signUpForm.tsx
@@ -116,20 +116,17 @@ export default function SignUp(){
                     </div>
                   </div>
 
-                  <div>
-                    <button
-                      type="submit"
-                      className="flex w-full justify-center rounded-md bg-btn px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-btn focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
-                      Sign up
-                    </button>
-                  </div>
-                </form>
-
-                
-              </div>
-
-                </>
+                    <div>
+                      <button
+                        type="submit"
+                        className="flex w-full justify-center rounded-md bg-btn px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-btn focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                      >
+                        Sign up
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </>
             )}
           </div>
         </div>
