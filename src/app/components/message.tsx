edbebf3b2ab43acbdd5ced32e350174c83f9808d
farbubby <farhan.mahbub@ucf.edu>
2023-08-27 17:15:55 -0400

    added the api fetching in the message component

--- src/app/components/message.tsx
+++ src/app/components/message.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 interface MessageProps {
   message: string;
 }
@@ -15,13 +18,39 @@ export function UserMessage({ message }: MessageProps) {
 }
 
 export function AIMessage({ message }: MessageProps) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [AImessage, setAImessage] = useState("");
+
+  useEffect(() => {
+    axios
+      .post("http://localhost:3001/message", JSON.stringify(message))
+      .then((res) => {
+        setAImessage(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(true);
+        setLoading(false);
+        setAImessage("Error in generating response");
+      });
+  }, [message]);
+
   return (
     <>
-      <div className="flex justify-start py-2 px-2 animate-fadeDown">
-        <div className="text-white bg-blue-600 rounded-xl p-4 max-w-2xl break-words">
-          <div>{message}</div>
+      {loading || error ? (
+        loading ? (
+          <div className="text-white">Loading...</div>
+        ) : (
+          <div className="text-white">{AImessage}</div>
+        )
+      ) : (
+        <div className="flex justify-start py-2 px-2 animate-fadeDown">
+          <div className="text-white bg-blue-600 rounded-xl p-4 max-w-2xl break-words">
+            <div>{AImessage}</div>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
